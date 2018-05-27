05a9896a90ee92d51fcdcadfc11600aa939dcd85
Adonias <Adonias@172.16.34.251>
2018-05-27 11:12:33 -0300

    Tipo: Nova Funcionalidade RF06 - Cadastro de Anamnese Alimentar Tarefa: Criar bean Tipo Alimento.

---
+++ src/main/java/br/unama/fabsoft/bean/TipoAlimento.java
@@ -0,0 +1,24 @@
+package br.unama.fabsoft.bean;
+
+public class TipoAlimento {
+
+	private int id;
+	private String descricao;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+}
