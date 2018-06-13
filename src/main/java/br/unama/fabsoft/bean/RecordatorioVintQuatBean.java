c6ac421acf2e3d57af483b810418c69769b8cb20
Marcela'Oliveira <Marcela'Oliveira@DESKTOP-NE2D132>
2018-06-13 14:40:36 -0300

    Tipo: RF08 (Recordatorio Vinte e Quatro Horas) Tarefa: Criação do Bean

--- src/main/java/br/unama/fabsoft/bean/RecordatorioVintQuatBean.java
+++ src/main/java/br/unama/fabsoft/bean/RecordatorioVintQuatBean.java
@@ -1,5 +1,50 @@
 package br.unama.fabsoft.bean;
 
-public class Bean {
-
+public class RecordatorioVintQuatBean {
+	private int id;
+	private String refeicao;
+	private float hora;
+	private String local;
+	private String alimento;
+	private double quantidade;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getRefeicao() {
+		return refeicao;
+	}
+	public void setRefeicao(String refeicao) {
+		this.refeicao = refeicao;
+	}
+	public float getHora() {
+		return hora;
+	}
+	public void setHora(float hora) {
+		this.hora = hora;
+	}
+	public String getLocal() {
+		return local;
+	}
+	public void setLocal(String local) {
+		this.local = local;
+	}
+	public String getAlimento() {
+		return alimento;
+	}
+	public void setAlimento(String alimento) {
+		this.alimento = alimento;
+	}
+	public double getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(double quantidade) {
+		this.quantidade = quantidade;
+	}
+	
+	
 }
