edcbefc33495ef99b439cbd923f7cc28f7735e96
rodrigo <rodrigo@DESKTOP-54DE7RK>
2018-06-23 11:18:49 -0300

    RF03-Cadastro de Avaliação Bioquímica Funcionalidade: Criação da Bean Tipo: Criação da Classe AlvaliacaoBioquimica

---
+++ src/main/java/br/unama/fabsoft/bean/AvaliacaoBioquimicaBean.java
@@ -0,0 +1,52 @@
+package br.unama.fabsoft.bean;
+
+import java.util.Date;
+
+public class AvaliacaoBioquimicaBean {
+	private int id_avalBioquimica;
+	private Paciente id_pessoa;
+	private String exameLab;
+	private String referencia;
+	private Date[] dataRealizacao = new Date[4];
+
+	public int getId_avalBioquimica() {
+		return id_avalBioquimica;
+	}
+
+	public void setId_avalBioquimica(int id_avalBioquimica) {
+		this.id_avalBioquimica = id_avalBioquimica;
+	}
+
+	public Paciente getId_pessoa() {
+		return id_pessoa;
+	}
+
+	public void setId_pessoa(Paciente id_pessoa) {
+		this.id_pessoa = id_pessoa;
+	}
+
+	public String getExameLab() {
+		return exameLab;
+	}
+
+	public void setExameLab(String exameLab) {
+		this.exameLab = exameLab;
+	}
+
+	public String getReferencia() {
+		return referencia;
+	}
+
+	public void setReferencia(String referencia) {
+		this.referencia = referencia;
+	}
+
+	public Date[] getDataRealizacao() {
+		return dataRealizacao;
+	}
+
+	public void setDataRealizacao(Date[] dataRealizacao) {
+		this.dataRealizacao = dataRealizacao;
+	}
+
+}
