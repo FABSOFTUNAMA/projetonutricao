9121817710a272635aed3d34a3a0e6bbaf3d3c19
26138744 <26138744@UNA007-L201-005.serlab.intra>
2018-11-19 17:48:39 -0200

    Classe de modelo Paciente (parcial)

---
+++ src/main/java/br/unama/fabsoft/Modelo/Paciente.java
@@ -0,0 +1,35 @@
+package br.unama.fabsoft.Modelo;
+
+public class Paciente {
+	private int numeroProntuario;
+	private String nome;
+	private char sexo;
+	private String dataNascimento;
+	private String estadoCivil;
+	private String telefone;
+	private String endereco;
+	private String municipio;
+	private String naturalidade;
+	private String etniaRegiliao;
+	private String email;
+	
+	private String rendaFamiliar;
+	private String composicaoFamiliar;
+	private String grauInstrucao;
+	private String profissao;
+	private String horarioTrabalho;
+	
+	private String acnMotivoConsulta;
+	private String acnTratamentoAnterior;
+	private String acnAlimentaFora;
+	private String acnAlergiaAlimentar;
+	private String acnIntoleranciaAlimentar;
+	private String acnTabuAlimentar;
+	private String acnAversaoAlimentar;
+	private String acnAtividadeFisicaNome;
+	private String acnAtividadeFisicaFrequencia;
+	private String acnAtividadeFisicaHorario;
+	private String acnIngestaoHidrica;
+	private String acnDiurese;
+	private String acnEvacuacao;
+}
