44151e80120608b1a336b3de5d7e89e5b3265e21
Henrique Arthur <5016325+henriquearthur@users.noreply.github.com>
2018-11-19 16:59:19 -0200

    Update app.js

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -6,6 +6,47 @@ $('input:radio[name="acn_tratamento_anterior"]').on('change', function(){
     }
 });
 
+$('input:radio[name="acn_alergia_alimentar"]').on('change', function(){
+    if ($(this).is(':checked') && $(this).val() == 's') {
+        $("#acn_alergia_alimentar_motivo").show();
+    } else {
+        $("#acn_alergia_alimentar_motivo").hide();
+    }
+});
+
+$('input:radio[name="acn_intolerancia_alimentar"]').on('change', function(){
+    if ($(this).is(':checked') && $(this).val() == 's') {
+        $("#acn_intolerancia_alimentar_motivo").show();
+    } else {
+        $("#acn_intolerancia_alimentar_motivo").hide();
+    }
+});
+
+$('input:radio[name="acn_tabu_alimentar"]').on('change', function(){
+    if ($(this).is(':checked') && $(this).val() == 's') {
+        $("#acn_tabu_alimentar_motivo").show();
+    } else {
+        $("#acn_tabu_alimentar_motivo").hide();
+    }
+});
+
+$('input:radio[name="acn_aversao_alimentar"]').on('change', function(){
+    if ($(this).is(':checked') && $(this).val() == 's') {
+        $("#acn_aversao_alimentar_motivo").show();
+    } else {
+        $("#acn_aversao_alimentar_motivo").hide();
+    }
+});
+
+$('input:radio[name="acn_atividade_fisica"]').on('change', function(){
+    if ($(this).is(':checked') && $(this).val() == 's') {
+        $("#acn_atividade_fisica_abre").show();
+    } else {
+        $("#acn_atividade_fisica_abre").hide();
+    }
+});
+
+
 function adicionaTudo(e){
     var exame = $("#exameLab").val();
     var referencia = $("#referencia").val();
