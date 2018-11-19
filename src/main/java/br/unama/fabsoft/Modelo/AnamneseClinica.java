148a281b8b4a74373675bc756679e772611825ba
26138744 <26138744@UNA007-L201-005.serlab.intra>
2018-11-19 18:14:47 -0200

    Getters e Setters

--- src/main/java/br/unama/fabsoft/Modelo/AnamneseClinica.java
+++ src/main/java/br/unama/fabsoft/Modelo/AnamneseClinica.java
@@ -14,4 +14,83 @@ public class AnamneseClinica {
 	private String ingestaoHidrica;
 	private String diurese;
 	private String evacuacao;
+	public String getMotivoConsulta() {
+		return motivoConsulta;
+	}
+	public void setMotivoConsulta(String motivoConsulta) {
+		this.motivoConsulta = motivoConsulta;
+	}
+	public String getTratamentoAnterior() {
+		return tratamentoAnterior;
+	}
+	public void setTratamentoAnterior(String tratamentoAnterior) {
+		this.tratamentoAnterior = tratamentoAnterior;
+	}
+	public String getAlimentaFora() {
+		return alimentaFora;
+	}
+	public void setAlimentaFora(String alimentaFora) {
+		this.alimentaFora = alimentaFora;
+	}
+	public String getAlergiaAlimentar() {
+		return alergiaAlimentar;
+	}
+	public void setAlergiaAlimentar(String alergiaAlimentar) {
+		this.alergiaAlimentar = alergiaAlimentar;
+	}
+	public String getIntoleranciaAlimentar() {
+		return intoleranciaAlimentar;
+	}
+	public void setIntoleranciaAlimentar(String intoleranciaAlimentar) {
+		this.intoleranciaAlimentar = intoleranciaAlimentar;
+	}
+	public String getTabuAlimentar() {
+		return tabuAlimentar;
+	}
+	public void setTabuAlimentar(String tabuAlimentar) {
+		this.tabuAlimentar = tabuAlimentar;
+	}
+	public String getAversaoAlimentar() {
+		return aversaoAlimentar;
+	}
+	public void setAversaoAlimentar(String aversaoAlimentar) {
+		this.aversaoAlimentar = aversaoAlimentar;
+	}
+	public String getAtividadeFisicaNome() {
+		return atividadeFisicaNome;
+	}
+	public void setAtividadeFisicaNome(String atividadeFisicaNome) {
+		this.atividadeFisicaNome = atividadeFisicaNome;
+	}
+	public String getAtividadeFisicaFrequencia() {
+		return atividadeFisicaFrequencia;
+	}
+	public void setAtividadeFisicaFrequencia(String atividadeFisicaFrequencia) {
+		this.atividadeFisicaFrequencia = atividadeFisicaFrequencia;
+	}
+	public String getAtividadeFisicaHorario() {
+		return atividadeFisicaHorario;
+	}
+	public void setAtividadeFisicaHorario(String atividadeFisicaHorario) {
+		this.atividadeFisicaHorario = atividadeFisicaHorario;
+	}
+	public String getIngestaoHidrica() {
+		return ingestaoHidrica;
+	}
+	public void setIngestaoHidrica(String ingestaoHidrica) {
+		this.ingestaoHidrica = ingestaoHidrica;
+	}
+	public String getDiurese() {
+		return diurese;
+	}
+	public void setDiurese(String diurese) {
+		this.diurese = diurese;
+	}
+	public String getEvacuacao() {
+		return evacuacao;
+	}
+	public void setEvacuacao(String evacuacao) {
+		this.evacuacao = evacuacao;
+	}
+	
 }
